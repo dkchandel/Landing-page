e017c5065128986a2241693a96b159b6dc2d278b
Dheeraj chandel <dheerajchandel376@gmail.com>
2025-07-13 12:06:24 +0530

    Updated title and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: "/Landing-page/",
+  base: "/Landing-page/",        // Base path for deployment
   plugins: [react(),tailwindcss()],
 })
